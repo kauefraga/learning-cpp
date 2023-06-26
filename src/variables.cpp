c222793c195b2ed7d079308a66cd6b3a9b5f52b0
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-06-26 20:17:41 -0300

    :sparkles: feat: develop a code about variables

---
+++ src/variables.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+/// @brief A variable assigned with the integer 10.
+int number = 10;
+
+float half = 0.5f;
+
+double one_fourth = 0.25;
+
+bool is_true = true;
+
+char end_of_line = '\n';
+
+std::string text = "Hello, world";
