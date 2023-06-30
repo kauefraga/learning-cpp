578be6aecb69f84bed214b8856199a91daac339c
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-06-30 20:54:58 -0300

    :sparkles: feat: add getters/setters and inheritance lessons

--- src/classes.cpp
+++ src/classes.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
 class Student {
+  private:
+    int money = 0.0f; // for private attributes, we will need getters and setters
   public:
     std::string name;
     int age;
     double gpa;
 
   // constructor (needs to be named exactly like the class name)
+  // you can overload constructor if you need to assign different parameters
   Student(std::string name, int age, double gpa) {
     this -> name = name;
     this -> age = age;
@@ -16,12 +19,31 @@ class Student {
   void read() {
     std::cout << "The student " << name << " is reading..." << '\n';
   }
+
+  // getter (returns the attribute type)
+  int get_money() {
+    return money;
+  }
+
+  // setter (doesn't return anything)
+  void set_money(int money) {
+    this -> money = money;
+  }
+};
+
+// Inheritance
+class Spongebob : public Student {
+  public:
+    std::string name = "Spongebob";
 };
 
 int main() {
   Student student("Patrick", 25, 3.4);
 
   student.read();
+
+  student.set_money(1000);
+  std::cout << student.get_money() << '\n';
 
   return 0;
 }
